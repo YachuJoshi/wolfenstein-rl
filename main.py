02e380ce0e7bf7ad085a31a19d978744ff16e0bf
Yachu Joshi <yachujoshi1234@gmail.com>
2023-01-03 15:25:14 +0545

    Code refactoring :recycle:

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ env = WolfensteinEnv()
 train_model(env=env, total_steps=400000)
 
 # Testing
-env = WolfensteinEnv(render_mode="human")
-MODEL_PATH = "./models/model_400000"
-test_model(env=env, model_path=MODEL_PATH)
+# env = WolfensteinEnv(render_mode="human")
+# MODEL_PATH = "./models/model_400000"
+# test_model(env=env, model_path=MODEL_PATH)
